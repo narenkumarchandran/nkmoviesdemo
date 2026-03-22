0be8f456dbf1bc0afb890e67ee0cc72b0906ad37
Narenkumar Chandran <narenkumarchandran@gmail.com>
2026-03-23 00:42:20 +0530

    fix: edge case fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API = axios.create({ baseURL: "http://localhost:5000/api" });
+export const API = axios.create({ baseURL: import.meta.env.VITE_API_URL||"http://localhost:5000/api" });
 
 // Attach token automatically
 API.interceptors.request.use((req) => {
